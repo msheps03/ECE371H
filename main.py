12f821d4b3483bd31339a3fcc63b0f5f0c9ea579
Daniel Lacayo <daniellacayo@Daniels-Air.hsd1.ma.comcast.net>
2021-11-30 11:18:47 -0500

    Creates model building methods, depends on saved model

--- main.py
+++ main.py
@@ -79,74 +79,75 @@ classes_dict = { 0:'Speed limit (20km/h)',
             41:'End of no passing',
             42:'End no passing veh > 3.5 tons' }
 
-'''
-# store data and labels in a list
-data =[]
-labels = []
-classes =43
-cur_path = os.getcwd()
 
 
 
-# preprocess images
-for i in range(classes):
-    path = os.path.join(cur_path,'Train',str(i))
-    images = os.listdir(path)
-    for a in images:
-        try:
-            # image = Image.open(path +'\\'+ a) # for pc
-            image = Image.open(path +'/'+ a) # for mac
-            image = image.resize((30,30))
-            # Resizing all images into 30*30
-            image =np.array(image)
-            data.append(image)
-            labels.append(i)
-        except Exception as e:
-            print(e)
 
-# convert lists into numpy arrays
-data = np.array(data)
-labels = np.array(labels)
+def pre_process():
+    # store data and labels in a list
+    data =[]
+    labels = []
+    classes =43
+    cur_path = os.getcwd()
 
-print(data.shape, labels.shape)
-# train the model?
-X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=0)
-print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+    # preprocess images
+    for i in range(classes):
+        path = os.path.join(cur_path,'Train',str(i))
+        images = os.listdir(path)
+        for a in images:
+            try:
+                # image = Image.open(path +'\\'+ a) # for pc
+                image = Image.open(path +'/'+ a) # for mac
+                image = image.resize((30,30))
+                # Resizing all images into 30*30
+                image =np.array(image)
+                data.append(image)
+                labels.append(i)
+            except Exception as e:
+                print(e)
+    # convert lists into numpy arrays and return arrays
+    return np.array(data), np.array(labels)
 
-# convert labels to onehot encoding
-y_train = to_categorical(y_train, 43)
-y_test = to_categorical(y_test, 43)
+def build_model():
+    data, labels = pre_process()
+    # print(data.shape, labels.shape)
 
-# build model
-model = Sequential()
-model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=X_train.shape[1:]))
-model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu'))
-model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Dropout(rate=0.25))
-model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
-model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Dropout(rate=0.25))
-model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(rate=0.5))
-# We have 43 classes that's why we have defined 43 in the dense
-model.add(Dense(43, activation='softmax'))
-print("Model built successfully")
+    # train test split
+    X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=0)
+    # print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
-#Compilation of the model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-# epochs = 20
-# history = model.fit(X_train, y_train, batch_size=32, epochs=epochs, validation_data=(X_test, y_test))
+    # convert labels to onehot encoding
+    y_train = to_categorical(y_train, 43)
+    y_test = to_categorical(y_test, 43)
 
-# testing
-# X_test, label = testing('Test.csv')
-# Y_pred = model.predict_classes(X_test)
-# print("Accuracy: ",accuracy_score(label, Y_pred))
+    # build model
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=X_train.shape[1:]))
+    model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(rate=0.25))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(rate=0.25))
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(rate=0.5))
+    # We have 43 classes that's why we have defined 43 in the dense
+    model.add(Dense(43, activation='softmax'))
 
-# save model
-model.save("./training/TSR.h5")
-'''
+    #Compilation of the model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # epochs = 20
+    # history = model.fit(X_train, y_train, batch_size=32, epochs=epochs, validation_data=(X_test, y_test))
+
+    # save model
+    model.save("./training/TSR.h5")
+    return
+
+# if no model exists, build model, otherwise leave commented out
+# build_model()
+
 # load model
 model = load_model('./training/TSR.h5')
 
